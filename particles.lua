b2a4b3a66cda50b6ede0d8e78cc35e67344e9002
garcia1000 <inaspanishway@gmail.com>
2015-12-14 08:01:14 +0800

    Higher quality code

--- particles.lua
+++ particles.lua
@@ -1,6 +1,5 @@
 local class = require 'middleclass'
 require 'utilities'
-draw_count = 0
 
 --[[---------------------------------------------------------------------------
                                 PARTICLE / FX CLASS
@@ -11,7 +10,7 @@ function Particle:initialize(image, image_size, sprite_size)
   self.image_size = {image:getDimensions()}
   self.sprite_size = sprite_size
   self.hitbox = {}
-  self.index = draw_count
+  draw_count = 0 -- each object gets a new index number, to prevent overwriting
 end
 
 function Particle:getDrawable(image_index, pos_h, pos_v)
@@ -25,6 +24,8 @@ end
 Dizzy = Particle:new(love.graphics.newImage('images/Dizzy.png'), {70, 50}, {70, 50})
 function Dizzy:loadFX(pos_h, pos_v)
   draw_count = draw_count + 1
+
+  -- write the animation frames to drawbuffer
   if not drawbuffer[frame] then drawbuffer[frame] = {} end
   drawbuffer[frame][draw_count] = Dizzy:getDrawable(0, pos_h, pos_v)
 end
@@ -34,27 +35,29 @@ WireSea = Particle:new(love.graphics.newImage('images/WireSea.png'), {610, 122},
 
 function WireSea:loadFX(pos_h, pos_v)
   draw_count = draw_count + 1
+
   local TIME_DIV = 3 -- advance the animation every TIME_DIV frames
   for i = frame, (frame + 14) do
     local index = math.floor((i - frame) / TIME_DIV) -- get the animation frame
 
+    -- write the animation frames to drawbuffer
     if not drawbuffer[i] then drawbuffer[i] = {} end
     drawbuffer[i][draw_count] = WireSea:getDrawable(index, pos_h, pos_v)
   end
 end
 
 -------------------------------- EXPLOSION ------------------------------------
-
 Explosion = Particle:new(love.graphics.newImage('images/Explosion.png'), {768, 64}, {64, 64})
 
 function Explosion:loadFX(pos_h, pos_v, vel_h, vel_v, friction, gravity)
   draw_count = draw_count + 1
-  print(draw_count)
+
   local TIME_DIV = 2 -- advance the animation every TIME_DIV frames
   for i = frame, (frame + 23) do
     local index = math.floor((i - frame) / TIME_DIV) -- get the animation frame
     local h_displacement = (vel_h / (friction - 1)) * math.exp((friction - 1) * index / TIME_DIV) - vel_h / (friction - 1)
 
+    -- write the animation frames to drawbuffer
     if not drawbuffer[i] then drawbuffer[i] = {} end
     drawbuffer[i][draw_count] = Explosion:getDrawable(index, pos_h + h_displacement, pos_v + (vel_v * index))
   end
