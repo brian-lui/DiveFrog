baac52a4118be37c99b2217e2670319cefe96ef8
garcia1000 <inaspanishway@gmail.com>
2015-12-15 20:44:07 +0800

    screen renamed to stage (entire playing field)

--- screen.lua
+++
@@ -1,7 +0,0 @@
-local screen = {}
-screen.widthPx = 800
-screen.heightPx = 600
-screen.floor = screen.heightPx - 50
-screen.left = 20
-screen.right = screen.widthPx - screen.left
-return screen
---
+++ stage.lua
@@ -0,0 +1,9 @@
+-- stage
+local stage = {}
+stage.width = 800
+stage.center = stage.width / 2
+stage.height = 600
+stage.floor = stage.height - 50
+stage.left = 20
+stage.right = stage.width - stage.left
+return stage
