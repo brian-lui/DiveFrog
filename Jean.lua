3455c582ebd4d62dd37bafbdbb8e1631433de3a9
garcia1000 <inaspanishway@gmail.com>
2016-01-04 21:08:16 +0800

    Preparing to separate characters into their own files

---
+++ Jean.lua
@@ -0,0 +1,254 @@
+local class = require 'middleclass'
+local stage = require 'stage' -- for checking floor/walls
+local window = require 'window'
+local music = require 'music' -- background music
+require 'utilities'
+require 'particles'
+require 'character'
+
+Jean = class('Jean', Fighter)
+function Jean:initialize(init_player, init_foe, init_super, init_dizzy, init_score)
+  Fighter.initialize(self, init_player, init_foe, init_super, init_dizzy, init_score)
+  self.icon = love.graphics.newImage('images/Jean/JeanIcon.png')
+  self.win_portrait = love.graphics.newImage('images/Jean/JeanPortrait.png')
+  self.win_quote = 'You must defeat "Wampire" to stand a chance.'
+  self.fighter_name = "Mustachioed Jean"
+  self.BGM = "JeanTheme.ogg"
+  self.stage_background = love.graphics.newImage('images/Jean/JeanBackground.jpg')
+  self.image = love.graphics.newImage('images/Jean/JeanTiles.png')
+  self.image_size = {1200, 200}
+  self.vel_multiple_super = 1.3
+  self.sprite_size = {150, 200}
+  self.default_gravity = 0.42
+  self.sprite_wallspace = 25 -- how many pixels to reduce when checking against stage wall
+  self.dandy = false
+  self.pilebunk_ok = false
+  self.pilebunking = false
+
+  -- sound effects
+  self.jump_sfx = "Jean/JeanJump.ogg"
+  self.attack_sfx = "Jean/JeanAttack.ogg"
+  self.got_hit_sfx = "Jean/JeanKO.ogg"
+  self.hit_sound_sfx = "Potatoes.ogg"
+  self.dandy_sfx = "Jean/JeanDandy.ogg"
+  self.pilebunker_sfx = "Jean/JeanBunker.ogg"
+  self.air_special_sfx = "Jean/JeanAirSpecial.ogg"
+
+  self.hurtboxes_standing = {
+    {L = 53, U = 27, R = 95, D = 50, Flag1 = "Mugshot"},
+    {L = 44, U = 51, R = 102, D = 79, Flag1 = "Mugshot"},
+    {L = 51, U = 85, R = 95, D = 101},
+    {L = 45, U = 105, R = 104, D = 142},
+    {L = 50, U = 143, R = 99, D = 172},
+    {L = 57, U = 173, R = 95, D = 190}}
+  self.hurtboxes_jumping  = {
+    {L = 53, U = 27, R = 95, D = 50, Flag1 = "Mugshot"},
+    {L = 44, U = 51, R = 102, D = 79, Flag1 = "Mugshot"},
+    {L = 51, U = 85, R = 95, D = 101},
+    {L = 45, U = 105, R = 104, D = 142},
+    {L = 50, U = 143, R = 99, D = 172},
+    {L = 57, U = 173, R = 95, D = 190}}
+  self.hurtboxes_falling = {
+    {L = 53, U = 27, R = 95, D = 50, Flag1 = "Mugshot"},
+    {L = 44, U = 51, R = 102, D = 79, Flag1 = "Mugshot"},
+    {L = 51, U = 85, R = 95, D = 101},
+    {L = 45, U = 105, R = 104, D = 142},
+    {L = 50, U = 143, R = 99, D = 172},
+    {L = 57, U = 173, R = 95, D = 190}}
+  self.hurtboxes_attacking  = {
+    {L = 10, U = 24, R = 57, D = 69, Flag1 = "Mugshot"},
+    {L = 61, U = 58, R = 77, D = 69},
+    {L = 31, U = 72, R = 83, D = 109},
+    {L = 42, U = 110, R = 93, D = 126},
+    {L = 61, U = 129, R = 116, D = 149},
+    {L = 118, U = 138, R = 131, D = 149},
+    {L = 62, U = 151, R = 145, D = 172}}
+  self.hurtboxes_dandy  = {
+    {L = 11, U = 33, R = 39, D = 76, Flag1 = "Mugshot"},
+    {L = 15, U = 82, R = 45, D = 129},
+    {L = 24, U = 131, R = 61, D = 151},
+    {L = 62, U = 142, R = 73, D = 151},
+    {L = 33, U = 152, R = 82, D = 166},
+    {L = 47, U = 167, R = 95, D = 186}}
+  self.hurtboxes_pilebunker = {
+    {L = 6, U = 36, R = 37, D = 71, Flag1 = "Mugshot"},
+    {L = 17, U = 68, R = 71, D = 137},
+    {L = 72, U = 87, R = 130, D = 92},
+    {L = 73, U = 128, R = 100, D = 137},
+    {L = 42, U = 140, R = 108, D = 187},
+    {L = 110, U = 152, R = 118, D = 187}}
+  self.hurtboxes_pilebunkerB = {
+    {L = 15, U = 36, R = 46, D = 71, Flag1 = "Mugshot"},
+    {L = 17, U = 68, R = 71, D = 137},
+    {L = 73, U = 128, R = 83, D = 137},
+    {L = 42, U = 140, R = 98, D = 187},
+    {L = 100, U = 165, R = 113, D = 180}}
+
+  self.hitboxes_attacking = {{L = 130, U = 154, R = 147, D = 172}}
+  self.hitboxes_pilebunker = {{L = 86, U = 85, R = 148, D = 92, Flag1 = "Wallsplat"}}
+
+  self:init2(init_player, init_foe, init_super, init_dizzy, init_score)
+end
+
+  function Jean:attack_key_press()
+    -- attack if in air and not already attacking.
+    if self.isInAir and not self.isAttacking and not self.dandy and not self.pilebunking and
+      (self.pos[2] + self.sprite_size[2] < stage.floor - 50 or
+      (self.vel[2] > 0 and self.pos[2] + self.sprite_size[2] < stage.floor - 30)) then
+        self.waiting = 3
+        self.waiting_state = "Attack"
+    -- dandy step replaces kickback, only do if in neutral state
+    elseif not self.isInAir and not self.dandy and not self.pilebunking then
+      self.waiting = 3
+      self.waiting_state = "Dandy"
+    -- pilebunk only if allowed conditions met
+    elseif not self.isInAir and self.dandy and self.pilebunk_ok then
+      self.waiting = 3
+      self.waiting_state = "Pilebunker"
+    end  
+    Fighter.attack_key_press(self) -- check for special move
+  end
+
+  function Jean:attack(h_vel, v_vel)
+    self.vel = {h_vel * self.facing, v_vel}
+    self.isAttacking = true
+    self:updateImage(4)
+    self.gravity = 0
+    self.current_hurtboxes = self.hurtboxes_attacking
+    self.current_hitboxes = self.hitboxes_attacking
+    if not self.isSupering then self.super = math.min(self.super + 8, 96) end
+  end
+
+
+  function Jean:dandyStep(h_vel) -- self.dandy is a backstep
+    self.dandy = true
+    self.vel[1] = h_vel * self.facing
+    self:updateImage(3)
+    self.current_hurtboxes = self.hurtboxes_dandy
+    self.isFrictionOn = false
+    if not self.isSupering then self.super = math.min(self.super + 4, 96) end
+  end
+
+  function Jean:pilebunk(h_vel)
+    self.pilebunk_ok = false
+    self.dandy = false
+    self.pilebunking = true -- to prevent dandy step or pilebunker while pilebunking
+    self.isAttacking = true -- needed to activate hitboxes
+    Explosion1:postLoadFX(self.pos[1] + 150, self.pos[2] + 50, self.facing * 2, self.shift * -50, 0, true)
+    Explosion2:preLoadFX(self.pos[1] + 200, self.pos[2] + 70, self.facing * 2, self.shift * -100, 5, true)
+    Explosion3:postLoadFX(self.pos[1] + 250, self.pos[2] + 30, self.facing * 2, self.shift * -150, 10, true)
+    Explosion1:preLoadFX(self.pos[1] + 190, self.pos[2] + 40, self.facing * 2, self.shift * -90, 13, true)
+    Explosion2:postLoadFX(self.pos[1] + 220, self.pos[2] + 80, self.facing * 2, self.shift * -120, 15, true)
+    Explosion3:preLoadFX(self.pos[1] + 230, self.pos[2] + 25, self.facing * 2, self.shift * -130, 17, true)
+
+    self.vel[1] = h_vel * self.facing
+    self:updateImage(6)
+    self.current_hurtboxes = self.hurtboxes_pilebunker
+    self.current_hitboxes = self.hitboxes_pilebunker
+    if not self.isSupering then self.super = math.min(self.super + 10, 96) end
+  end
+
+  function Jean:air_special()
+    if self.isSupering then
+      self.waiting_state = ""
+      writeSound(self.air_special_sfx)
+      self:jump(0, -36)
+    elseif self.super >= 8 and not self.isAttacking then
+      self.super = self.super - 8
+      self.waiting_state = ""
+      writeSound(self.air_special_sfx)
+      self:jump(0, -36)
+    end
+  end    
+
+  function Jean:ground_special()
+    if self.isSupering and (self.dandy or self.pilebunking) and math.abs(self.vel[1]) < 18 then
+      self.super = self.super - 8
+      self.waiting_state = ""
+      WireSea:postLoadFX(self.center, self.pos[2] + self.sprite_size[2] / 2, self.facing, self.shift * WireSea.width, 0, true)
+      self:land()
+      p1:setFrozen(10)
+      p2:setFrozen(10)
+    elseif self.super >= 16 and (self.dandy or self.pilebunking) and math.abs(self.vel[1]) < 18 then
+      self.super = self.super - 16
+      self.waiting_state = ""
+      WireSea:postLoadFX(self.center, self.pos[2] + self.sprite_size[2] / 2, self.facing, self.shift * WireSea.width, 0, true)
+      self:land()
+      p1:setFrozen(10)
+      p2:setFrozen(10)
+    end
+  end
+
+  function Jean:stateCheck()
+    if self.waiting > 0 then
+      self.waiting = self.waiting - 1
+      if self.waiting == 0 and self.waiting_state == "Attack" then 
+      self.waiting_state = ""
+        self:attack(9.6, 9.6)
+        writeSound(self.attack_sfx)
+      end
+      if self.waiting == 0 and self.waiting_state == "Dandy" then
+        self.waiting_state = ""
+        self:dandyStep(-36)        
+        writeSound(self.dandy_sfx)
+      end
+      if self.waiting == 0 and self.waiting_state == "Pilebunker" then
+        self.waiting_state = ""
+        self:pilebunk(56)        
+        writeSound(self.pilebunker_sfx)
+      end
+      if self.waiting == 0 and self.waiting_state == "Jump" then
+        self.waiting_state = ""
+        self:jump(0, 14)
+        writeSound(self.jump_sfx)
+      end      
+    end
+  end
+
+  function Jean:extraStuff()
+    if self.dandy or self.pilebunking then self.vel[1] = self.vel[1] * 0.77 end -- custom friction
+    -- during dandy step's slowing end part, allow pilebunker
+    if self.dandy and math.abs(self.vel[1]) >= 0.1 and math.abs(self.vel[1]) < 3.6 then
+      self.pilebunk_ok = true
+    else self.pilebunk_ok = false 
+    end 
+    
+    -- when dandy step is almost stopped, return to neutral
+    if self.dandy and math.abs(self.vel[1]) < 0.02 then
+      self.dandy = false
+      self.pilebunk_ok = false
+      self.isFrictionOn = false
+      self.vel[1] = 0
+      self:updateImage(0)
+      self.current_hurtboxes = self.hurtboxes_standing
+    end
+
+    -- stop pilebunking, and change to recovery frames
+    if self.pilebunking and math.abs(self.vel[1]) >= 0.001 and math.abs(self.vel[1]) < 1.2 then
+      self.isAttacking = false
+      self:updateImage(7)
+      self.current_hurtboxes = self.hurtboxes_pilebunkerB
+      self.current_hitboxes = self.hitboxes_neutral
+    end
+
+    -- change from recovery to neutral
+    if self.pilebunking and math.abs(self.vel[1]) < 0.001 and not self.hasWon then
+      self.pilebunking = false
+      self.dandy = false
+      self.isFrictionOn = false
+      self.vel[1] = 0
+      self:updateImage(0)
+      self.current_hurtboxes = self.hurtboxes_standing
+    end
+  end
+
+function Jean:gotHit(type)
+  Fighter.gotHit(self, type)
+  self.pilebunking = false
+  self.dandy = false
+  self.isFrictionOn = true
+end
+
+function Jean:getNeutral() -- don't check for facing if in dandy/pilebunker
+  return not self.isKO and not self.isAttacking and not self.dandy and not self.pilebunking
+end
---
+++ Konrad.lua
@@ -0,0 +1,183 @@
+local class = require 'middleclass'
+local stage = require 'stage' -- for checking floor/walls
+local window = require 'window'
+local music = require 'music' -- background music
+require 'character'
+require 'utilities'
+require 'particles'
+
+Konrad = class('Konrad', Fighter)
+function Konrad:initialize(init_player, init_foe, init_super, init_dizzy, init_score)
+  Fighter.initialize(self, init_player, init_foe, init_super, init_dizzy, init_score)
+  self.fighter_name = "Konrad"
+  self.icon = love.graphics.newImage('images/Konrad/KonradIcon.png')
+  self.win_portrait = love.graphics.newImage('images/Konrad/KonradPortrait.png')
+  self.win_quote = "You have been defeated by Konrad the talking frog with a cape who plays poker."
+  self.stage_background = love.graphics.newImage('images/Konrad/KonradBackground.jpg')
+  self.BGM = "KonradTheme.ogg"
+  self.image = love.graphics.newImage('images/Konrad/KonradTiles.png')
+  self.image_size = {1200, 200}
+  self.sprite_size = {200, 200}
+  self.sprite_wallspace = 50 -- how many pixels to reduce when checking against stage wall
+  self.default_gravity = 0.36
+  self.double_jump = false
+  
+  --lists of hitboxes and hurtboxes for the relevant sprites. format is LEFT, TOP, RIGHT, BOTTOM, relative to top left corner of sprite.
+  self.hurtboxes_standing = {
+    {L = 78, U = 28, R = 119, D = 50, Flag1 = "Mugshot"},
+    {L = 69, U = 51, R = 124, D = 79, Flag1 = "Mugshot"},
+    {L = 76, U = 85, R = 120, D = 101},
+    {L = 70, U = 105, R = 129, D = 142},
+    {L = 75, U = 143, R = 124, D = 172},
+    {L = 82, U = 173, R = 120, D = 190}}
+  self.hurtboxes_jumping  = {
+    {L = 78, U = 28, R = 119, D = 50, Flag1 = "Mugshot"},
+    {L = 69, U = 51, R = 124, D = 79, Flag1 = "Mugshot"},
+    {L = 76, U = 85, R = 120, D = 101},
+    {L = 70, U = 105, R = 129, D = 142},
+    {L = 75, U = 143, R = 124, D = 172},
+    {L = 82, U = 173, R = 120, D = 190}}
+  self.hurtboxes_falling = {
+    {L = 78, U = 28, R = 119, D = 50, Flag1 = "Mugshot"},
+    {L = 69, U = 51, R = 124, D = 79, Flag1 = "Mugshot"},
+    {L = 76, U = 85, R = 120, D = 101},
+    {L = 70, U = 105, R = 129, D = 142},
+    {L = 75, U = 143, R = 124, D = 172},
+    {L = 82, U = 173, R = 120, D = 190}}
+  self.hurtboxes_attacking  = {
+    {L = 67, U = 20, R = 109, D = 59, Flag1 = "Mugshot"},
+    {L = 75, U = 60, R = 104, D = 103},
+    {L = 68, U = 104, R = 91, D = 135},
+    {L = 100, U = 105, R = 114, D = 136},
+    {L = 111, U = 137, R = 128, D = 157},
+    {L = 125, U = 158, R = 138, D = 183}}
+  self.hurtboxes_kickback  = {
+    {L = 82, U = 25, R = 123, D = 71, Flag1 = "Mugshot"},
+    {L = 71, U = 41, R = 128, D = 72, Flag1 = "Mugshot"},
+    {L = 70, U = 73, R = 119, D = 165},
+    {L = 72, U = 166, R = 111, D = 182}}
+
+  self.hitboxes_attacking = {{L = 119, U = 166, R = 137, D = 183}}
+  self.hitboxes_hyperkick = {{L = 119, U = 166, R = 137, D = 183, Flag1 = "Fire"}}
+
+  self.hyperkicking = false
+
+  -- sound effects
+  self.jump_sfx = "Konrad/KonradJump.ogg"
+  self.attack_sfx = "Konrad/KonradAttack.ogg"
+  self.got_hit_sfx = "Konrad/KonradKO.ogg"
+  self.hit_sound_sfx = "Potatoes.ogg"
+  self.ground_special_sfx = "Konrad/KonradHyperJump.ogg"
+
+  self:init2(init_player, init_foe, init_super, init_dizzy, init_score)
+end
+
+  function Konrad:jump_key_press()
+    if self.isInAir and not self.isAttacking and not self.double_jump then
+      self.waiting = 3
+      self.waiting_state = "DoubleJump"
+      self.double_jump = true
+    end
+    Fighter.jump_key_press(self) -- check for ground jump or special move
+  end
+
+  function Konrad:attack_key_press()
+    -- attack if in air and not already attacking and either: >50 above floor, or landing and >30 above.
+    if self.isInAir and not self.isAttacking and 
+      (self.pos[2] + self.sprite_size[2] < stage.floor - 50 or
+      (self.vel[2] > 0 and self.pos[2] + self.sprite_size[2] < stage.floor - 30)) then
+        self.waiting = 3
+        self.waiting_state = "Attack"
+    -- if on ground, kickback
+    elseif not self.isInAir then
+      self.waiting = 3
+      self.waiting_state = "Kickback"
+    end
+    Fighter.attack_key_press(self) -- check for special move
+  end
+
+  function Konrad:air_special()
+    if self.super >= 16 and not self.isAttacking and not self.isSupering and
+    self.pos[2] + self.sprite_size[2] < stage.floor - 50 then
+      self.super = self.super - 16
+      self.waiting_state = ""
+      HyperKickFlames:playSound()
+      self.vel = {14 * self.facing, 19}
+      self.isAttacking = true  
+      self.hyperkicking = true
+      self:updateImage(4)
+      self.gravity = 0
+      self.current_hurtboxes = self.hurtboxes_attacking
+      self.current_hitboxes = self.hitboxes_hyperkick
+    end
+  end
+
+  function Konrad:ground_special()
+    if self.super >= 16 and not self.isSupering then
+      self.super = self.super - 16
+      self.waiting_state = ""
+      writeSound(self.ground_special_sfx)
+      self:jump(0, 29, 1.2)
+    end
+  end
+
+  function Konrad:jump(h_vel, v_vel, gravity)
+    self.isInAir = true
+    self.gravity = gravity
+    self.vel = {h_vel * self.facing, -v_vel}
+    self:updateImage(1)
+    self.current_hurtboxes = self.hurtboxes_jumping
+  end
+
+  function Konrad:land() -- called when character lands on floor
+    self.isInAir = false
+    self.isAttacking = false
+    self.double_jump = false
+    self.hyperkicking = false
+    if not self.isKO then
+      self.vel = {0, 0}
+      self:updateImage(0)
+      self.current_hurtboxes = self.hurtboxes_standing
+    end
+  end
+
+  function Konrad:stateCheck()
+    if self.waiting > 0 then
+      self.waiting = self.waiting - 1
+      if self.waiting == 0 and self.waiting_state == "Jump" then
+        self.waiting_state = ""
+        self:jump(0, 14, self.default_gravity)
+        writeSound(self.jump_sfx)
+        JumpDust:postLoadFX(self.center, self.pos[2] + self.sprite_size[2] - 30, self.facing, self.shift * JumpDust.width)
+      end
+      if self.waiting == 0 and self.waiting_state == "DoubleJump" then
+        self.waiting_state = ""
+        self:jump(4.8, 4.8, self.default_gravity)
+        DoubleJumpDust:postLoadFX(self.pos[1] + 30,
+          self.pos[2] + self.sprite_size[2] - DoubleJumpDust.height,
+          self.facing, self.shift * (self.sprite_size[1] - 60), 0, true)
+      end
+      if self.waiting == 0 and self.waiting_state == "Attack" then 
+        self.waiting_state = ""
+        self:attack(7.2, 9.6)
+        writeSound(self.attack_sfx)
+      end
+      if self.waiting == 0 and self.waiting_state == "Kickback" then
+        self.waiting_state = ""
+        self:kickback(-7.2, 7.2)
+        writeSound(self.jump_sfx)
+      end
+    end
+  end
+
+  function Konrad:victoryPose()
+    if frame - round_end_frame == 60 then self.hyperkicking = false end
+    Fighter.victoryPose(self)
+  end
+
+  function Konrad:extraStuff()
+    if self.hyperkicking and not self.isKO then
+      HyperKickFlames:postRepeatFX(self.pos[1], self.pos[2], self.facing, self.shift * HyperKickFlames.width)
+    end
+  end
+
